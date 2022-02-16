e07870576cda6d216fdf852303650da1aefe6f7f
Diego A <diego8fcb@hotmail.com>
2022-02-16 20:11:33 +0100

    Add files via upload

---
+++ 2021/day2/challenge_four.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+index_of_space = lambda srs: [i.find(" ") for i in srs]
+
+def main():
+    data = pd.read_csv("input_data", header=None)
+    data.rename(columns={0:"instructions"}, inplace=True)
+    data["i_number"] = data["instructions"].str[-2:]
+
+    data["i_number"] = (data["i_number"].astype("int"))
+
+    aim = 0
+    ver_sum = 0
+    hor_sum = 0
+
+    for i in range(0, data.shape[0]):
+        if data.iloc[i, 0].find("down") != -1:
+            aim += data.iloc[i, 1]
+        if data.iloc[i, 0].find("up") != -1:
+            aim -= data.iloc[i, 1]
+        if data.iloc[i, 0].find("forward") != -1:
+            hor_sum += data.iloc[i, 1]
+            ver_sum += aim*data.iloc[i, 1]
+
+    print("ver_sum={}, hor_sum={}. Multiplied results in={}".format(ver_sum, hor_sum, ver_sum*hor_sum))
+
+
+    print(data.head(5))
+
+if __name__ == "__main__":
+    main()
